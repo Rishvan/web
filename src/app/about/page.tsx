b8af219ff3fc5143c7b05de072e4281017cd30a0
rishvan-global <rishvan.m@globalgov.ae>
2024-06-09 09:41:00 +0530

    Create page.tsx

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import data from "../../data/data.json";
+import Link from "next/link";
+import { silkscreen } from "@/common/function";
+
+export default function Home() {
+  return (
+    <section className="h-screen text-white px-4 md:px-28">
+      <p>about page</p>
+    </section>
+  );
+}
