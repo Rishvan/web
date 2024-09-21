6d25d8cc41d2253376b3fb2aa477dd151091d8b9
Rishvan <rishvan.m@globalgov.ae>
2024-09-21 10:23:35 +0400

    home page updated

--- src/app/home/home copy.tsx
+++ src/app/home/home copy.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import data from "../../data/data.json";
 import Link from "next/link";
@@ -24,88 +23,8 @@ export default function HomePage() {
     screenWidth >= 640 ? data["designations"] : data["mobile-designations"];
 
   return (
-    <section className="h-screen max-w-[2800px] text-white px-4 md:px-28 flex justify-between flex-col items-center">
-      <div></div>
-      <div className="flex flex-col items-center">
-        <div
-          className={`${silkscreen.className} text-5xl md:text-7xl text-center flex`}
-        >
-          {data.name.split("").map((e) => {
-            return (
-              <p
-                key={e}
-                className={
-                  e == "."
-                    ? "animate-bounce"
-                    : "hover:scale-110" + `ease-linear duration-300`
-                }
-              >
-                {e ?? ""}
-              </p>
-            );
-          })}
-        </div>
-        <ul
-          className={
-            "flex w-full items-center  justify-center md:justify-start gap-6 pt-2 pb-4 md:pb-0 text-sm md:text-xl"
-          }
-        >
-          {currentDesignations.map((designation: string, i) => {
-            return (
-              <li
-                className={`text-gray-300 hover:text-white ${i == 0 ? "md:pr-1" : "md:px-1 list-disc"} ${
-                  screenWidth >= 640 ? "" : "text-xl"
-                }`}
-                key={i}
-              >
-                {designation}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="flex md:justify-between w-full flex-col md:flex-row gap-2 pb-5 ">
-        <div className="flex gap-3 md:gap-12">
-          {data.cards &&
-            data.cards.map((card: Cards, i) => {
-              return (
-                <div key={i}>
-                  <Link href={`${card.url}`}>
-                    <p className="text-gray-300 text-sm hover:text-white md:text-md  transition-transform">
-                      {card.title}
-                    </p>
-                  </Link>
-                </div>
-              );
-            })}
-        </div>
-        <div className="flex flex-col md:flex-row gap-2 md:gap-12">
-          <div>
-            {data.social_icons.map((item: SocialTcons, i) => {
-              return (
-                <Link href={`${item.url}`} target="_blank" key={i}>
-                  <button
-                    className="w-4 md:w-5 aspect-square mx-2 hover:scale-125 transition-transform"
-                    key={i}
-                  >
-                    <Image
-                      src={`assets/icons/${item.icon}.svg`}
-                      width={500}
-                      height={500}
-                      alt="Picture of the author"
-                    />
-                  </button>
-                </Link>
-              );
-            })}
-          </div>
-          <div className="text-gray-300 text-sm md:text-md   hover:text-white">
-            <span>{"© Rishvan M. 2024 All rights reserved."}</span>
-          </div>
-        </div>
-      </div>
-
-      {/* <div className="flex flex-col justify-center md:items-start h-full w-full items-center">
+    <section className="h-screen text-white px-4 md:px-28">
+      <div className="flex flex-col justify-center md:items-start h-full w-full items-center">
         <div
           className={`${silkscreen.className} text-5xl md:text-6xl text-center flex`}
         >
@@ -180,7 +99,7 @@ export default function HomePage() {
               );
             })}
         </div>
-      </div> */}
+      </div>
     </section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-"use client";
-import Image from "next/image";
-import data from "../data/data.json";
-import Link from "next/link";
-import { silkscreen } from "@/common/function";
-import { useEffect, useState } from "react";
 import HomePage from "./home/home";
 
 export default function Home() {
