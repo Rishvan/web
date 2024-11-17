2ac9758bedc684b404b1067ef2de62c7090e5191
Rishvan <rishvan.m@globalgov.ae>
2024-11-17 23:18:44 +0400

    about udpated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,27 +1,60 @@
+"use client";
 import _ from "lodash";
 import data from "@/data/data.json";
 import Header from "@/components/header";
+import { motion, useTransform } from "framer-motion";
 
 export default function About() {
   const { aboutMe } = data;
-
+  const items = [
+    "html",
+    "css",
+    "react js",
+    "sass",
+    "next js",
+    "flutter",
+    "redux",
+    "gitlab",
+    "laravel",
+    "blade",
+    "node js",
+    "github",
+    "type script",
+    "java script",
+  ];
   return (
-    <section className="h-screen overflow-hidden">
+    <section className="h-screen overflow-hidden relative">
       <Header title="About" />
-      <div className="h-[85vh] overflow-scroll w-full flex flex-col-reverse  lg:grid grid-cols-3 p-16 gap-12">
-        <div className=" w-full h-full col-span-2 text-sm lg:text-xl text-justify text-white lg:p-16 ">
-          {aboutMe}
-        </div>
-
-        <div
-          className={`bg-blue-400s w-full h-full flex justify-center items-center `}
-        >
-          {/* <img
-            src={`/assets/projects/animal.jpg`}
-            alt="Card Background"
-            className="h-full md:h-[1/3] lg:h-[2/3] aspect-[4/5] object-cover transition-all duration-300"
-          /> */}
-          .
+      <div className="h-[85vh] overflow-scroll w-full  text-white">
+        <div className="flex flex-col h-full justify-between pb-12">
+          <div className="p-8 w-full h-full text-sm lg:text-xl lg:w-3/4  text-justify lg:p-16 ">
+            {aboutMe}
+          </div>
+          {/* <div>languages using</div> */}
+          <div className="w-full overflow-x-hidden py-12">
+            <motion.div
+              className="flex gap-12 md:gap-64 w-fit"
+              animate={{ x: ["0%", "-50%"] }}
+              transition={{ duration: 30, ease: "linear", repeat: Infinity }}
+            >
+              {items.concat(items).map((e, i) => (
+                <p key={i} className="text-6xl md:text-[100px] w-max uppercase">
+                  {e}
+                </p>
+              ))}
+            </motion.div>
+            <motion.div
+              className="flex gap-12 md:gap-64 w-fit"
+              animate={{ x: ["-50%", "0%"] }}
+              transition={{ duration: 30, ease: "linear", repeat: Infinity }}
+            >
+              {items.concat(items).map((e, i) => (
+                <p key={i} className="text-6xl md:text-[100px] w-max uppercase">
+                  {e}
+                </p>
+              ))}
+            </motion.div>
+          </div>
         </div>
       </div>
     </section>
